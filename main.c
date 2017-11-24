2380e60d6ebe4ce6637cd55ef02949ec5bdbbe67
juanmi <juanmi@juanmi-MS-7817>
2017-11-24 09:39:32 +0100

    2 commit

--- main.c
+++ main.c
@@ -10,19 +10,33 @@ int main(void) {
 //recorrer array y verificar que es numero.
 
 char num[9];
-//int numero;
+
 int i;
+int numero;
+
 printf("introduzca numero: \t \n ");
 scanf ("\t \n %s", &num);
 //atoi(num);
 //numero=num;
 //numval=num;
-//printf("el numero introducido es %i \t\n ",numero);
+printf("el numero introducido es %s \t\n ",num);
     //le decimos el maximo del vector y hasta que llegue al final de la escritrura (/0) qu se situa detras del numero escrito.
 for (i=0; ((i<9)&&(num[i]!='\0')); i++){ 
-    printf("%c \n",num[i]);
+    //printf(" num %c \t \n",num [i]);
+   
+    if (isdigit(num[i])) {
+        numero = atoi(num);
+        //printf("\t %i \n",numero);
+    }
+        else{
+            printf("no es numero \n");
+        break;
+   }
+   
     
-}
+    }
+
+
     
     
     
